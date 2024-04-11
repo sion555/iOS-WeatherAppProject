41c101572456486a70e5c2bf0bee26375d47be53
hbs <hbs09222@gmail.com>
2024-04-11 11:24:07 +0900

    test tokki2

--- iosProject-5/Main/ViewController.swift
+++ iosProject-5/Main/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
         print("tokki")
-        
+        print("tokki2")
         
         configureLocationManager()
         
